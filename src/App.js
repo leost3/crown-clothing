bb8acc175cf25d488e79ef6adefe05489a1c0077
leost3 <leost3@gmail.com>
2019-08-09 10:03:19 -0400

    sign in form and button components finished and styled

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-
-
+import SignInAndSignUp from './pages/sign-in-and-sign-out/sign-in-and-sign-up.component';
 
 const App = () => (
     <div>
@@ -16,6 +15,7 @@ const App = () => (
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/shop' exact component={ShopPage} />
+        <Route path='/signin' exact component={SignInAndSignUp} />
       </Switch>
     </div>
 )
